33cc78da5d091930ce369d80c64b973e651562c6
Helga <89998178494@mail.ru>
2022-08-08 22:08:54 +0300

    feat(save): кнопка сохранить заработала !

--- src/ToDoList/ToDoListDisplay/TDLEdit/TDLEdit.tsx
+++ src/ToDoList/ToDoListDisplay/TDLEdit/TDLEdit.tsx
@@ -10,17 +10,26 @@ export const TDLEdit = ({ item }: Iprops) => {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setText(event.target.value);
   };
-  // const saveTodo = (id: number) => {
-  //   newTodo = [...item].map(item=>(
-  //       if(item.id==id){item.text=value}
-  //   ))
-  // };
+  const handleOpenEditMode = () => {
+    setIsEdit(true);
+  };
+  const saveTodo = (item: any) => {
+    console.log(item);
+    if (item.id == item) {
+      item.text = text;
+      return item;
+    } else {
+      setText(text);
+    }
+    setIsEdit(false);
+  };
+
   return isEdit ? (
     <div>
       <input onChange={handleChange} value={text} />
-      <button onClick={() => saveTodo(item.id)}>Сохранить</button>
+      <button onClick={saveTodo}>Сохранить</button>
     </div>
   ) : (
-    <div onClick={() => setIsEdit(true)}>{text}</div>
+    <div onClick={handleOpenEditMode}>{text}</div>
   );
 };
