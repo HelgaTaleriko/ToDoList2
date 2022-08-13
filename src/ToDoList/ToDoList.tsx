54700d8dfe834fba0ff1cfd7e9010162f83d869d
Helga <89998178494@mail.ru>
2022-08-13 18:53:03 +0300

    feat(save): кнопка сохранить заработала снова!

--- src/ToDoList/ToDoList.tsx
+++ src/ToDoList/ToDoList.tsx
@@ -10,18 +10,60 @@ export type ItemType = {
 export const ToDoList = () => {
   const [items, setItems] = useState<ItemType[]>([{ text: "test", id: 0 }]);
   const addText = (text: string) => {
-    setItems([...items, { text: text, id: new Date().valueOf() }]);
+    //создаем элементы массива
+    const newItem: ItemType = {
+      text: text,
+      id: new Date().valueOf(),
+      status: "toDo",
+    };
+    //создаем массив
+    const newArray = [...items, newItem];
+    // const array2 = [...items];
+    // array2.push(newItem);
+    // setItems устанавливает новое значение items
+    setItems(newArray);
   };
   const minusText = () => {
     setItems([]);
   };
-  console.log(items);
+  // console.log(items);
+
+  //переписать deleteitem
   const deleteItem = (id: number) => {
-    const newItems = items.filter((item) => {
+    const filteredItems = items.filter((item) => {
       return item.id !== id;
     });
-    setItems(newItems);
+    setItems(filteredItems);
   };
+  //решение Миши №1:
+  const saveTodo = (text: string, id: number) => {
+    //создаем элемент массива
+    const newItem: ItemType = {
+      text: text,
+      id: id,
+      status: "toDo",
+    };
+    console.log(newItem);
+    //создаем фильтрованый массив без старого
+
+    const filtredItems = items.filter((item) => {
+      return item.id !== id;
+    });
+    console.log(filtredItems);
+    //добавляем в фильтрованый массив новое значение
+    const newArray = [...filtredItems, newItem];
+    //устанавливаем setItems
+    setItems(newArray);
+    console.log(newArray);
+    console.log(items);
+    //
+    //решение черех findIndex:
+    // найти элемент , который = id элемента массива
+    //
+    // нашли элемент , присваниваем ему новое значение
+    //
+  };
+  //из массива найти элемент и поменять в этом элементе значение
   return (
     <div>
       {/*<ToDoListDisplay*/}
@@ -36,7 +78,16 @@ export const ToDoList = () => {
       {/*  deleteItem={deleteItem}*/}
       {/*  items={items.filter((item) => item.status === "completed")}*/}
       {/*/>*/}
-      <ToDoListDisplay items={items} deleteItem={deleteItem} />
+      <ToDoListDisplay
+        items={items}
+        deleteItem={deleteItem}
+        saveTodo={saveTodo}
+      />
+      <ToDoListDisplay
+        items={items}
+        deleteItem={deleteItem}
+        saveTodo={saveTodo}
+      />
       <ToDoListButtons addText={addText} minusText={minusText} />
     </div>
   );
--- src/ToDoList/ToDoListDisplay/TDLEdit/TDLEdit.tsx
+++ src/ToDoList/ToDoListDisplay/TDLEdit/TDLEdit.tsx
@@ -1,34 +1,31 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import { ItemType } from "../../ToDoList";
 
 export type Iprops = {
   item: ItemType;
+  saveTodo: (text: string, id: number) => void;
 };
-export const TDLEdit = ({ item }: Iprops) => {
+export const TDLEdit = ({ item, saveTodo }: Iprops) => {
   const [text, setText] = useState(item.text);
-  const [isEdit, setIsEdit] = useState(null);
+  const [isEdit, setIsEdit] = useState(false);
+  const handleClickEdit = () => {
+    saveTodo(text, item.id);
+    setIsEdit(false);
+  };
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setText(event.target.value);
   };
-  const editTodo = (item: any) => {
-    setIsEdit(item);
-  };
-  const saveTodo = (item: any) => {
-    if (item.id == item) {
-      item.text = text;
-      return item;
-    } else {
-      setText(text);
-    }
-    setIsEdit(null);
+  const handleOpenEditMode = () => {
+    setIsEdit(true);
   };
 
   return isEdit ? (
     <div>
       <input onChange={handleChange} value={text} />
-      <button onClick={saveTodo}>Сохранить</button>
+      <button onClick={handleClickEdit}>Сохранить</button>
     </div>
   ) : (
-    <div onClick={editTodo}>{text}</div>
+    <div onClick={handleOpenEditMode}>{item.text}</div>
   );
 };
